f5059923fad49929c7ef19f30d2b182a07ab962e
Subrata Mondal <subratasubha2@gmail.com>
2024-02-22 17:27:29 +0530

    Setup Database Models

---
+++ backend/models.py
@@ -0,0 +1,41 @@
+"""Database Models"""
+
+import datetime as _dt
+
+import sqlalchemy as _sql
+import sqlalchemy.orm as _orm
+import passlib.hash as _hash
+
+import backend.database as _database
+
+
+class User(_database.Base):
+    """User's Database Table"""
+
+    __tablename__ = "users"
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    email = _sql.Column(_sql.String, unique=True, index=True)
+    hashed_password = _sql.Column(_sql.String)
+
+    leads = _orm.relationship("Lead", back_populates="owner")
+
+    def verify_password(self, password: str):
+        """Password Verification"""
+        return _hash.bcrypt.verify(password, self.hashed_password)
+
+
+class Lead(_database.Base):
+    """Lead's Database Table"""
+
+    __tablename__ = "leads"
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    owner_id = _sql.Column(_sql.Integer, _sql.ForeignKey("users.id"))
+    firstname = _sql.Column(_sql.String, index=True)
+    lastname = _sql.Column(_sql.String, index=True)
+    email = _sql.Column(_sql.String, unique=True, index=True)
+    company = _sql.Column(_sql.String, index=True, default="")
+    note = _sql.Column(_sql.String, default="")
+    date_created = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
+    date_last_updated = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
+
+    owner = _orm.relationship("User", back_populates="leads")
