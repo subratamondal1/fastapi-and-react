6880399e40fba84630156b4a3f4919256039cbfb
Subrata Mondal <subratasubha2@gmail.com>
2024-02-23 20:02:45 +0530

    Added User Authentication with JWT (Jason Web Tokens)

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 """Entry Point"""
-
 import fastapi as _fastapi
 import fastapi.security as _security
 
@@ -8,17 +7,83 @@ import sqlalchemy.orm as _orm
 import services as _services
 import schemas as _schemas
 
+import models as _models
+import auth.jwt_handler as _jwt_handler
+import auth.jwt_bearer as _jwt_bearer
+
+posts = [
+    {
+        "id":1,
+        "title": "penguin 🐧",
+        "content": "penguins are group of...."
+    },
+    {
+        "id":2,
+        "title": "tiger 🐯",
+        "content": "tigers are group of...."
+    },
+    {
+        "id":3,
+        "title": "snake 🐍",
+        "content": "snakes are group of...."
+    },
+]
+
+users = []
+
 app = _fastapi.FastAPI()
 
-@app.post(path="/api/users")
-async def create_user(user:_schemas.UserCreate, db:_orm.Session=_fastapi.Depends(_services.get_db)):
-    db_user = await _services.get_user_by_email(email=user.email, db=db)
-    if db_user:
-        raise _fastapi.exceptions.HTTPException(
+@app.get(path="/", tags=["test"])
+def greet():
+    return {"Hello":"World"}
+
+# get all posts
+@app.get(path="/posts", tags=["posts"])
+def get_all_posts():
+    return {"data":posts}
+
+# get single post by id
+@app.get(path=f"/posts/{id}", tags=["posts"])
+def get_one_post(id:int):
+    if id > len(posts):
+        return _fastapi.exceptions.HTTPException(
             status_code=_fastapi.status.HTTP_400_BAD_REQUEST,
-            detail="Email already exists!!!"
+            detail="Id doesn't exists..."
         )
-    return await _services.create_user(user=user, db=db)
+    for post in posts:
+        if post["id"] == id:
+            return {
+                "data":post
+            }
+# create a new user
+@app.post(path="/user/signup", tags=["user"])
+def user_signup(user:_models.UserSchema = _fastapi.Body(default=None)):
+    users.append(user)
+    return _jwt_handler.signJWT(userID=user.email)
+
+def check_user_existence(data:_models.UserLoginSchema):
+    for user in users:
+        if (user.email == data.email) and (user.password == data.password):
+            return True
+        return False
+
+@app.post(path="/user/signin", tags=["user"])
+def user_login(user:_models.UserLoginSchema=_fastapi.Body(default=None)):
+    if check_user_existence(user):
+        return _jwt_handler.signJWT(userID=user.email)
+    return _fastapi.exceptions.HTTPException(
+        status_code=_fastapi.status.HTTP_400_BAD_REQUEST,
+        detail="Invalid Credentials"
+    )
+
+@app.post(path="/posts", dependencies=[_fastapi.Depends(_jwt_bearer.JwtBearer())], tags=["posts"])
+def add_post(post: _models.PostSchema):
+    post.id = len(posts) + 1
+    posts.append(post.model_dump())
+    return {
+        "info": "Post Added"
+    }
+    
 
 if __name__ == "__main__":
     import uvicorn as _uvicorn
--- backend/models.py
+++ backend/models.py
@@ -3,12 +3,54 @@ in your database (e.g., users, posts, comments)"""
 
 import datetime as _dt
 
+import pydantic as _pydantic
+
 import sqlalchemy as _sql
 import sqlalchemy.orm as _orm
+
 import passlib.hash as _hash
 
 import database as _database
 
+class PostSchema(_pydantic.BaseModel):
+    id: int = _pydantic.Field(default=None)
+    title: str = _pydantic.Field(default=None)
+    content: str = _pydantic.Field(default=None)
+
+    class Config:
+        schema_extra = {
+            "post_demo": {
+                "title": "Some title, for sampling",
+                "content": "Some content"
+            }
+        }
+class UserSchema(_pydantic.BaseModel):
+    firstname:str = _pydantic.Field(default=None)
+    lastname:str = _pydantic.Field(default=None)
+    email: _pydantic.EmailStr = _pydantic.Field(default=None)
+    password: str = _pydantic.Field(default=None)
+
+    class Config:
+        the_schema = {
+            "user_demo": {
+                "firstname":"Subrata",
+                "lastname": "Mondal",
+                "email":"subrata@gmail.com",
+                "password":"@1234"
+            }
+        }
+
+class UserLoginSchema(_pydantic.BaseModel):
+    email: _pydantic.EmailStr = _pydantic.Field(default=None)
+    password: str = _pydantic.Field(default=None)
+
+    class Config:
+        the_schema = {
+            "user_demo": {
+                "email":"subrata@gmail.com",
+                "password":"@1234"
+            }
+        }
 
 class User(_database.Base):
     """User's Database Table"""
